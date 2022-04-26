3f557ab5303625cc6d216f378e23d5f9039e9f7b
harikrishnanea-iitm <harikrishnanea@alumni.iitm.ac.in>
2022-04-26 11:07:15 +0530

    display individual svg with string

--- lib/main.dart
+++ lib/main.dart
@@ -22,8 +22,8 @@ class MyApp extends StatefulWidget {
 
 class MyAppState extends State<MyApp> {
   final List<String> svgBox = [
-    '''<svg width="800" height="650"><rect x="10" y="20" width="150" height="150" style="fill:white;stroke:green;stroke-width:5;fill-opacity:0.0;stroke-opacity:0.9" /></svg>''',
-    '''<svg width="800" height="650"><rect  x="100" y="200" width="200" height="150" style="fill:white;stroke:blue;stroke-width:5;fill-opacity:0.0;stroke-opacity:0.9" /></svg>'''
+    '<svg width="800" height="650"><rect x="10" y="20" width="150" height="150" style="fill:white;stroke:green;stroke-width:5;fill-opacity:0.0;stroke-opacity:0.9" /></svg>',
+    '<svg width="800" height="650"><rect  x="100" y="200" width="200" height="150" style="fill:white;stroke:blue;stroke-width:5;fill-opacity:0.0;stroke-opacity:0.9" /></svg>'
   ];
 
   final List<String> svgAsset = [
@@ -31,7 +31,23 @@ class MyAppState extends State<MyApp> {
     "assets/images/box1.svg"
   ];
 
+  final String blankSvg = '<svg width="800" height="650"></svg>';
+  final String blankSvgAsset = "assets/images/blank.svg";
+
+  String box0 = '<svg width="800" height="650"></svg>';
+  String box1 = '<svg width="800" height="650"></svg>';
+
   List<bool> buttonSel = [false, false];
+
+  @override
+  initState() {
+    super.initState();
+    fetchButtonState();
+  }
+
+  fetchButtonState() async {
+    print("fetch state");
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -57,12 +73,32 @@ class MyAppState extends State<MyApp> {
                     fit: BoxFit.none,
                   ),
                 ),
+                child: Stack(children: [
+                  SvgPicture.string(box0),
+                  SvgPicture.string(box1)
+                  // CustomPaint(painter: svgPainter(svgBox0, buttonSel[0])),
+                  // CustomPaint(painter: svgPainter(svgBox1, buttonSel[1]))
+                ]),
               ),
               ToggleButtons(
                 children: const [Icon(Icons.filter_1), Icon(Icons.filter_2)],
                 onPressed: (int index) {
                   setState(() {
                     buttonSel[index] = !buttonSel[index];
+
+                    if (buttonSel[0]) {
+                      box0 = svgBox[0];
+                    } else {
+                      box0 = blankSvg;
+                    }
+
+                    if (buttonSel[1]) {
+                      box1 = svgBox[1];
+                    } else {
+                      box1 = blankSvg;
+                    }
+
+                    // fetchButtonState();
                   });
                 },
                 isSelected: buttonSel,
